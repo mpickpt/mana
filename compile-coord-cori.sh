f3ef7956bd2d459f62bb0b1b8be30f02e712525e
Rohan Garg <rhn.grg@gmail.com>
2020-05-18 21:45:10 -0700

    Remove compile-coord-cori.sh script

--- compile-coord-cori.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd src/
-CC -DHAVE_CONFIG_H -I. -I../include  -I../include -I../jalib -Wall -DDEBUG -fPIC \
-   -std=c++11 -g3 -O0 -DDEBUG -MT dmtcp_coordinator.o -MD -MP -MF .deps/dmtcp_coordinator.Tpo \
-   -c -o dmtcp_coordinator.o dmtcp_coordinator.cpp
-# Next line no longer needed.  'make -j' does it.
-# g++ -g3 -O0 -o ../bin/dmtcp_coordinator dmtcp_coordinator.o lookup_service.o restartscript.o \
-#       	 libdmtcpinternal.a libjalib.a libnohijack.a -lpthread -lrt
