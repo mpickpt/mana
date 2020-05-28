29eef6922af5b0c8e38c67cf4c019ef5d60e1c1b
Gene Cooperman <gene@ccs.neu.edu>
2020-05-28 04:45:35 -0700

    setLhMemRange: conditional to use avail fixed addr

--- src/mtcp/mtcp_split_process.c
+++ src/mtcp/mtcp_split_process.c
@@ -241,8 +241,13 @@ setLhMemRange()
 
   int found = getMappedArea(&area, "[stack]");
   if (found) {
+#if !defined(USE_MANA_LH_FIXED_ADDRESS)
     lh_mem_range.start = (VA)area.addr - TWO_GB;
     lh_mem_range.end = (VA)area.addr - ONE_GB;
+#else
+    lh_mem_range.start = 0x2aab00000000;
+    lh_mem_range.end =   0x2aab00000000 + ONE_GB;
+#endif
   } else {
     DPRINTF("Failed to find [stack] memory segment\n");
     mtcp_abort();
