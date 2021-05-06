0914f9a431bfbd0ac90f815193115c2d5b5fcb50
Gene Cooperman <gene@ccs.neu.edu>
2021-05-06 13:17:37 -0700

    mpi-proxy-split/test/ : Add some 'fflush(stdout)'

--- contrib/mpi-proxy-split/test/mpi_hello_world.c
+++ contrib/mpi-proxy-split/test/mpi_hello_world.c
@@ -33,6 +33,11 @@ int main(int argc, char** argv)
   //int name_len;
   //MPI_Get_processor_name(processor_name, &name_len);
 
+  // Print off a hello world message
+  printf("Hello world from processor %s, rank %d out of %d processors\n",
+         processor_name, world_rank, world_size);
+  fflush(stdout);
+
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   assert(world_rank == original_rank);
