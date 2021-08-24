dmtcp/dmtcp

mtcp_restart.c: fix vdso/vvar overlap w/ orig addr
 * If vvar or vdso overlaps with their pre-checkpoint addresses, and
   are not the same addresses, we now try to move vvar to original
   address, if that does not create an address conflict.  After that,
   it must be safe to move vds0 to its original address.
 * The FIXME comment describes how to fix this fully, by first moving
   to a temporary address, but this is good enough for now.  If an error
   still occurs, the error message refers to this FIXME comment.

src/mtcp/mtcp_restart.c
@@ -142,6 +142,7 @@ static void restart_fast_path(void);
 static void restart_slow_path(void);
 static int doAreasOverlap(VA addr1, size_t size1, VA addr2, size_t size2);
 static int hasOverlappingMapping(VA addr, size_t size);
+static int mremap_move(void *dest, void *src, size_t size);
 static void remapMtcpRestartToReservedArea(RestoreInfo *rinfo);
 static void mtcp_simulateread(int fd, MtcpHeader *mtcpHdr);
 void restore_libc(ThreadTLSInfo *tlsInfo,
@@ -793,18 +794,31 @@ unmap_memory_areas_and_restore_vdso(RestoreInfo *rinfo)
   }
 
   // Check for overlap between newer and older vDSO/vvar sections.
-  if (doAreasOverlap(vdsoStart, vdsoEnd - vdsoStart,
+  if (
+#if 0
+      doAreasOverlap(vdsoStart, vdsoEnd - vdsoStart,
                      rinfo->vdsoStart, rinfo->vdsoEnd - rinfo->vdsoStart) ||
       doAreasOverlap(vdsoStart, vdsoEnd - vdsoStart,
                      rinfo->vvarStart, rinfo->vvarEnd - rinfo->vvarStart) ||
       doAreasOverlap(vvarStart, vvarEnd - vvarStart,
                      rinfo->vdsoStart, rinfo->vdsoEnd - rinfo->vdsoStart) ||
       doAreasOverlap(vdsoStart, vdsoEnd - vdsoStart,
-                     rinfo->vvarStart, rinfo->vvarEnd - rinfo->vvarStart)) {
-    MTCP_PRINTF("*** MTCP Error: Overlapping addresses for older and newer\n"
+                     rinfo->vvarStart, rinfo->vvarEnd - rinfo->vvarStart)
+#else
+      // We will move vvar first, if original vvar doesn't overlap with
+      // current vdso,  After that, it should be possible to move vdso to its
+      // original position.
+      doAreasOverlap(rinfo->vvarStart, vvarEnd - vvarStart,
+                     vdsoStart, vdsoEnd - vdsoStart)
+#endif
+     ) // FIXME:  We can temporarily move vvar or vdso to a third,
+       //         non-conflicting location, if a future kernel causes
+       //         this error condition to happen.
+  { MTCP_PRINTF("*** MTCP Error: Overlapping addresses for older and newer\n"
                 "                vDSO/vvar sections.\n"
                 "      vdsoStart: %p vdsoEnd: %p vvarStart: %p vvarEnd: %p\n"
-                "rinfo:vdsoStart: %p vdsoEnd: %p vvarStart: %p vvarEnd: %p\n",
+                "rinfo:vdsoStart: %p vdsoEnd: %p vvarStart: %p vvarEnd: %p\n"
+                "(SEE FIXME comment in source code for how to fix this.)\n",
                 vdsoStart,
                 vdsoEnd,
                 vvarStart,
@@ -816,21 +830,47 @@ unmap_memory_areas_and_restore_vdso(RestoreInfo *rinfo)
     mtcp_abort();
   }
 
-  if (vdsoStart != NULL) {
-    void *vdso = mtcp_sys_mremap(vdsoStart,
-                                 vdsoEnd - vdsoStart,
-                                 vdsoEnd - vdsoStart,
-                                 MREMAP_FIXED | MREMAP_MAYMOVE,
-                                 rinfo->vdsoStart);
-    if (vdso == MAP_FAILED) {
-      MTCP_PRINTF("***Error: failed to mremap vdso; errno: %d.\n",
+  // Move vvar to original location, followed by same for vdso
+  if (vvarStart != NULL) {
+    int rc = mremap_move(rinfo->vvarStart, vvarStart, vvarEnd - vvarStart);
+    if (rc == -1) {
+      MTCP_PRINTF("***Error: failed to remap vvarStart to old value "
+                  "%p -> %p); errno: %d.\n",
+                  vvarStart, rinfo->vvarStart, mtcp_sys_errno);
+      mtcp_abort();
+    }
+#if defined(__i386__)
+    // FIXME: For clarity of code, move this i386-specific code to a function.
+    vvar = mtcp_sys_mmap(vvarStart, vvarEnd - vvarStart,
+                         PROT_EXEC | PROT_WRITE | PROT_READ,
+                         MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    if (vvar == MAP_FAILED) {
+      MTCP_PRINTF("***Error: failed to mremap vvar; errno: %d\n",
                   mtcp_sys_errno);
       mtcp_abort();
     }
-    MTCP_ASSERT(vdso == rinfo->vdsoStart);
+    MTCP_ASSERT(vvar == vvarStart);
+    // On i386, only the first page is readable. Reading beyond that
+    // results in a bus error.
+    // Arguably, this is a bug in the kernel, since /proc/*/maps indicates
+    // that both pages of vvar memory have read permission.
+    // This issue arose due to a change in the Linux kernel pproximately in
+    // version 4.0
+    // TODO: Find a way to automatically detect the readable bytes.
+    mtcp_memcpy(vvarStart, rinfo->vvarStart, MTCP_PAGE_SIZE);
+#endif /* if defined(__i386__) */
+  }
 
+  if (vdsoStart != NULL) {
+    int rc = mremap_move(rinfo->vdsoStart, vdsoStart, vdsoEnd - vdsoStart);
+    if (rc == -1) {
+      MTCP_PRINTF("***Error: failed to remap vdsoStart to old value "
+                  "%p -> %p); errno: %d.\n",
+                  vdsoStart, rinfo->vdsoStart, mtcp_sys_errno);
+      mtcp_abort();
+    }
 #if defined(__i386__)
-
+    // FIXME: For clarity of code, move this i386-specific code toa function.
     // In commit dec2c26c1eb13eb1c12edfdc9e8e811e4cc0e3c2 , the mremap
     // code above was added, and then caused a segfault on restart for
     // __i386__ in CentOS 7.  In that case ENABLE_VDSO_CHECK was not defined.
@@ -862,40 +902,6 @@ unmap_memory_areas_and_restore_vdso(RestoreInfo *rinfo)
     }
     MTCP_ASSERT(vdso == vdsoStart);
     mtcp_memcpy(vdsoStart, rinfo->vdsoStart, vdsoEnd - vdsoStart);
-#endif /* if defined(__i386__) */
-  }
-
-  if (vvarStart != NULL) {
-    void *vvar = mtcp_sys_mremap(vvarStart,
-                                 vvarEnd - vvarStart,
-                                 vvarEnd - vvarStart,
-                                 MREMAP_FIXED | MREMAP_MAYMOVE,
-                                 rinfo->vvarStart);
-    if (vvar == MAP_FAILED) {
-      MTCP_PRINTF("***Error: failed to mremap vvar; errno: %d.\n",
-                  mtcp_sys_errno);
-      mtcp_abort();
-    }
-    MTCP_ASSERT(vvar == rinfo->vvarStart);
-
-#if defined(__i386__)
-    vvar = mtcp_sys_mmap(vvarStart, vvarEnd - vvarStart,
-                         PROT_EXEC | PROT_WRITE | PROT_READ,
-                         MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
-    if (vvar == MAP_FAILED) {
-      MTCP_PRINTF("***Error: failed to mremap vvar; errno: %d\n",
-                  mtcp_sys_errno);
-      mtcp_abort();
-    }
-    MTCP_ASSERT(vvar == vvarStart);
-    // On i386, only the first page is readable. Reading beyond that
-    // results in a bus error.
-    // Arguably, this is a bug in the kernel, since /proc/*/maps indicates
-    // that both pages of vvar memory have read permission.
-    // This issue arose due to a change in the Linux kernel pproximately in
-    // version 4.0
-    // TODO: Find a way to automatically detect the readable bytes.
-    mtcp_memcpy(vvarStart, rinfo->vvarStart, MTCP_PAGE_SIZE);
 #endif /* if defined(__i386__) */
   }
 }
@@ -1277,6 +1283,28 @@ hasOverlappingMapping(VA addr, size_t size)
   }
   mtcp_sys_close(mapsfd);
   return ret;
+}
+
+NO_OPTIMIZE
+static int
+mremap_move(void *dest, void *src, size_t size) {
+  int mtcp_sys_errno;
+  if (dest == src) {
+    return 0; // Success
+  }
+  void *rc = mtcp_sys_mremap(src, size, size, MREMAP_FIXED | MREMAP_MAYMOVE,
+                             dest);
+  if (rc == dest) {
+    return 0; // Success
+  } else if (rc == MAP_FAILED) {
+    MTCP_PRINTF("***Error: failed to mremap; errno: %d.\n",
+                mtcp_sys_errno);
+    return -1; // Error
+  } else {
+    // We failed to move the memory to 'dest'.  Undo it.
+    mremap_move(src, rc, size);
+    return -1; // Error
+  }
 }
 
 // This is the entrypoint to the binary. We'll need it for adding symbol file.

mpickpt/mana

src/mtcp/mtcp_restart.c
@@ -107,6 +107,7 @@ static void restart_fast_path(void);
 static void restart_slow_path(void);
 static int doAreasOverlap(VA addr1, size_t size1, VA addr2, size_t size2);
 static int hasOverlappingMapping(VA addr, size_t size);
+static int mremap_move(void *dest, void *src, size_t size);
 static void getTextAddr(VA *textAddr, size_t *size);
 static void mtcp_simulateread(int fd, MtcpHeader *mtcpHdr);
 void restore_libc(ThreadTLSInfo *tlsInfo,
@@ -1278,6 +1279,8 @@ restorememoryareas(RestoreInfo *rinfo_ptr, LowerHalfInfo_t *linfo_ptr)
       mtcp_sys_getpid()
     );
     restore_info.post_restart_debug(readTime, restore_info.mtcp_restart_pause);
+    // volatile int dummy = 1;
+    // while (dummy);
   } else {
     restore_info.post_restart(readTime);
   }
@@ -1416,18 +1419,31 @@ unmap_memory_areas_and_restore_vdso(RestoreInfo *rinfo, LowerHalfInfo_t *lh_info
   }
 
   // Check for overlap between newer and older vDSO/vvar sections.
-  if (doAreasOverlap(vdsoStart, vdsoEnd - vdsoStart,
+  if (
+#if 0
+      doAreasOverlap(vdsoStart, vdsoEnd - vdsoStart,
                      rinfo->vdsoStart, rinfo->vdsoEnd - rinfo->vdsoStart) ||
       doAreasOverlap(vdsoStart, vdsoEnd - vdsoStart,
                      rinfo->vvarStart, rinfo->vvarEnd - rinfo->vvarStart) ||
       doAreasOverlap(vvarStart, vvarEnd - vvarStart,
                      rinfo->vdsoStart, rinfo->vdsoEnd - rinfo->vdsoStart) ||
       doAreasOverlap(vdsoStart, vdsoEnd - vdsoStart,
-                     rinfo->vvarStart, rinfo->vvarEnd - rinfo->vvarStart)) {
-    MTCP_PRINTF("*** MTCP Error: Overlapping addresses for older and newer\n"
+                     rinfo->vvarStart, rinfo->vvarEnd - rinfo->vvarStart)
+#else
+      // We will move vvar first, if original vvar doesn't overlap with
+      // current vdso.  After that, it should be possible to move vdso to its
+      // original position.
+      doAreasOverlap(rinfo->vvarStart, vvarEnd - vvarStart,
+                     vdsoStart, vdsoEnd - vdsoStart)
+#endif
+     ) // FIXME:  We can temporarily move vvar or vdso to a third,
+       //         non-conflicting location, if a future kernel causes
+       //         this error condition to happen.
+  { MTCP_PRINTF("*** MTCP Error: Overlapping addresses for older and newer\n"
                 "                vDSO/vvar sections.\n"
                 "      vdsoStart: %p vdsoEnd: %p vvarStart: %p vvarEnd: %p\n"
-                "rinfo:vdsoStart: %p vdsoEnd: %p vvarStart: %p vvarEnd: %p\n",
+                "rinfo:vdsoStart: %p vdsoEnd: %p vvarStart: %p vvarEnd: %p\n"
+                "(SEE FIXME comment in source code for how to fix this.)\n",
                 vdsoStart,
                 vdsoEnd,
                 vvarStart,
@@ -1439,21 +1455,47 @@ unmap_memory_areas_and_restore_vdso(RestoreInfo *rinfo, LowerHalfInfo_t *lh_info
     mtcp_abort();
   }
 
-  if (vdsoStart != NULL) {
-    void *vdso = mtcp_sys_mremap(vdsoStart,
-                                 vdsoEnd - vdsoStart,
-                                 vdsoEnd - vdsoStart,
-                                 MREMAP_FIXED | MREMAP_MAYMOVE,
-                                 rinfo->vdsoStart);
-    if (vdso == MAP_FAILED) {
-      MTCP_PRINTF("***Error: failed to mremap vdso; errno: %d.\n",
+  // Move vvar to original location, followed by same for vdso
+  if (vvarStart != NULL) {
+    int rc = mremap_move(rinfo->vvarStart, vvarStart, vvarEnd - vvarStart);
+    if (rc == -1) {
+      MTCP_PRINTF("***Error: failed to remap vvarStart to old value "
+                  "%p -> %p); errno: %d.\n",
+                  vvarStart, rinfo->vvarStart, mtcp_sys_errno);
+      mtcp_abort();
+    }
+#if defined(__i386__)
+    // FIXME: For clarity of code, move this i386-specific code to a function.
+    vvar = mmap_fixed_noreplace(vvarStart, vvarEnd - vvarStart,
+                                PROT_EXEC | PROT_WRITE | PROT_READ,
+                               MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    if (vvar == MAP_FAILED) {
+      MTCP_PRINTF("***Error: failed to mremap vvar; errno: %d\n",
                   mtcp_sys_errno);
       mtcp_abort();
     }
-    MTCP_ASSERT(vdso == rinfo->vdsoStart);
+    MTCP_ASSERT(vvar == vvarStart);
+    // On i386, only the first page is readable. Reading beyond that
+    // results in a bus error.
+    // Arguably, this is a bug in the kernel, since /proc/*/maps indicates
+    // that both pages of vvar memory have read permission.
+    // This issue arose due to a change in the Linux kernel pproximately in
+    // version 4.0
+    // TODO: Find a way to automatically detect the readable bytes.
+    mtcp_memcpy(vvarStart, rinfo->vvarStart, MTCP_PAGE_SIZE);
+#endif /* if defined(__i386__) */
+  }
 
+  if (vdsoStart != NULL) {
+    int rc = mremap_move(rinfo->vdsoStart, vdsoStart, vdsoEnd - vdsoStart);
+    if (rc == -1) {
+      MTCP_PRINTF("***Error: failed to remap vdsoStart to old value "
+                  "%p -> %p); errno: %d.\n",
+                  vdsoStart, rinfo->vdsoStart, mtcp_sys_errno);
+      mtcp_abort();
+    }
 #if defined(__i386__)
-
+    // FIXME: For clarity of code, move this i386-specific code to a function.
     // In commit dec2c26c1eb13eb1c12edfdc9e8e811e4cc0e3c2 , the mremap
     // code above was added, and then caused a segfault on restart for
     // __i386__ in CentOS 7.  In that case ENABLE_VDSO_CHECK was not defined.
@@ -1485,40 +1527,6 @@ unmap_memory_areas_and_restore_vdso(RestoreInfo *rinfo, LowerHalfInfo_t *lh_info
     }
     MTCP_ASSERT(vdso == vdsoStart);
     mtcp_memcpy(vdsoStart, rinfo->vdsoStart, vdsoEnd - vdsoStart);
-#endif /* if defined(__i386__) */
-  }
-
-  if (vvarStart != NULL) {
-    void *vvar = mtcp_sys_mremap(vvarStart,
-                                 vvarEnd - vvarStart,
-                                 vvarEnd - vvarStart,
-                                 MREMAP_FIXED | MREMAP_MAYMOVE,
-                                 rinfo->vvarStart);
-    if (vvar == MAP_FAILED) {
-      MTCP_PRINTF("***Error: failed to mremap vvar; errno: %d.\n",
-                  mtcp_sys_errno);
-      mtcp_abort();
-    }
-    MTCP_ASSERT(vvar == rinfo->vvarStart);
-
-#if defined(__i386__)
-    vvar = mmap_fixed_noreplace(vvarStart, vvarEnd - vvarStart,
-                         PROT_EXEC | PROT_WRITE | PROT_READ,
-                         MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
-    if (vvar == MAP_FAILED) {
-      MTCP_PRINTF("***Error: failed to mremap vvar; errno: %d\n",
-                  mtcp_sys_errno);
-      mtcp_abort();
-    }
-    MTCP_ASSERT(vvar == vvarStart);
-    // On i386, only the first page is readable. Reading beyond that
-    // results in a bus error.
-    // Arguably, this is a bug in the kernel, since /proc/*/maps indicates
-    // that both pages of vvar memory have read permission.
-    // This issue arose due to a change in the Linux kernel pproximately in
-    // version 4.0
-    // TODO: Find a way to automatically detect the readable bytes.
-    mtcp_memcpy(vvarStart, rinfo->vvarStart, MTCP_PAGE_SIZE);
 #endif /* if defined(__i386__) */
   }
 }
@@ -1899,6 +1907,28 @@ hasOverlappingMapping(VA addr, size_t size)
 
 // XXX: Define this on systems with older LD
 #define OLDER_LD
+
+NO_OPTIMIZE
+static int
+mremap_move(void *dest, void *src, size_t size) {
+  int mtcp_sys_errno;
+  if (dest == src) {
+    return 0; // Success
+  }
+  void *rc = mtcp_sys_mremap(src, size, size, MREMAP_FIXED | MREMAP_MAYMOVE,
+                             dest);
+  if (rc == dest) {
+    return 0; // Success
+  } else if (rc == MAP_FAILED) {
+    MTCP_PRINTF("***Error: failed to mremap; errno: %d.\n",
+                mtcp_sys_errno);
+    return -1; // Error
+  } else {
+    // We failed to move the memory to 'dest'.  Undo it.
+    mremap_move(src, rc, size);
+    return -1; // Error
+  }
+}
 
 NO_OPTIMIZE
 static void