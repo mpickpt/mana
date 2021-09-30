01a14691a2d414aa28a85545acd7428c15e84686
Yao Xu <syxuyao@outlook.com>
2021-09-30 13:22:08 -0700

    Translate MPI_{Alloc,Free}_mem to malloc and free
      Memories allocated by the lower half will be discarded at
      checkpoint. So we translate MPI_Alloc_mem and MPI_Free_mem
      to malloc and free to keep them in the image file. This may
      slow down the program.

--- contrib/mpi-proxy-split/mpi-wrappers/mpi_win_wrappers.cpp
+++ contrib/mpi-proxy-split/mpi-wrappers/mpi_win_wrappers.cpp
@@ -8,24 +8,22 @@
 #include "mpi_nextfunc.h"
 
 USER_DEFINED_WRAPPER(int, Alloc_mem, (MPI_Aint) size, (MPI_Info) info,
-                     (void *) baseptr) {
-  int retval;
-  DMTCP_PLUGIN_DISABLE_CKPT();
-  JUMP_TO_LOWER_HALF(lh_info.fsaddr);
-  retval = NEXT_FUNC(Alloc_mem)(size, info, baseptr);
-  RETURN_TO_UPPER_HALF();
-  DMTCP_PLUGIN_ENABLE_CKPT();
-  return retval;
+                     (void *) baseptr)
+{
+  // Since memory allocated by the lower half will be discarded during
+  // checkpoint, we need to translate MPI_Alloc_mem and MPI_Free_mem
+  // to malloc and free. This may slow down the program.
+  *(void**)baseptr = malloc(size * sizeof(MPI_Aint));
+  return MPI_SUCCESS;
 }
 
-USER_DEFINED_WRAPPER(int, Free_mem, (void *) baseptr) {
-  int retval;
-  DMTCP_PLUGIN_DISABLE_CKPT();
-  JUMP_TO_LOWER_HALF(lh_info.fsaddr);
-  retval = NEXT_FUNC(Free_mem)(baseptr);
-  RETURN_TO_UPPER_HALF();
-  DMTCP_PLUGIN_ENABLE_CKPT();
-  return retval;
+USER_DEFINED_WRAPPER(int, Free_mem, (void *) baseptr)
+{
+  // Since memory allocated by the lower half will be discarded during
+  // checkpoint, we need to translate MPI_Alloc_mem and MPI_Free_mem
+  // to malloc and free. This may slow down the program.
+  free(baseptr);
+  return MPI_SUCCESS;
 }
 
 PMPI_IMPL(int, MPI_Alloc_mem, MPI_Aint size, MPI_Info info, void *baseptr)
