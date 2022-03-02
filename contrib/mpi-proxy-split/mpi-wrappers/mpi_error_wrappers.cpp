5359702881469386d8bc199037e164da60a51cbd
fyshhh <illio.suardi@gmail.com>
2022-03-02 12:56:40 -0800

    Implement MPI_Error related function wrappers

---
+++ contrib/mpi-proxy-split/mpi-wrappers/mpi_error_wrappers.cpp
@@ -0,0 +1,60 @@
+/****************************************************************************
+ *   Copyright (C) 2019-2022 by Illio Suardi                                *
+ *   illio@u.nus.edu                                                        *
+ *                                                                          *
+ *  This file is part of DMTCP.                                             *
+ *                                                                          *
+ *  DMTCP is free software: you can redistribute it and/or                  *
+ *  modify it under the terms of the GNU Lesser General Public License as   *
+ *  published by the Free Software Foundation, either version 3 of the      *
+ *  License, or (at your option) any later version.                         *
+ *                                                                          *
+ *  DMTCP is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU Lesser General Public License for more details.                     *
+ *                                                                          *
+ *  You should have received a copy of the GNU Lesser General Public        *
+ *  License in the files COPYING and COPYING.LESSER.  If not, see           *
+ *  <http://www.gnu.org/licenses/>.                                         *
+ ****************************************************************************/
+
+#include "mpi_plugin.h"
+#include "config.h"
+#include "dmtcp.h"
+#include "util.h"
+#include "jassert.h"
+#include "jfilesystem.h"
+#include "protectedfds.h"
+
+#include "mpi_nextfunc.h"
+#include "record-replay.h"
+#include "virtual-ids.h"
+
+USER_DEFINED_WRAPPER(int, Error_class, (int) errorcode, (int *) errorclass)
+{
+  int retval;
+  DMTCP_PLUGIN_DISABLE_CKPT();
+  JUMP_TO_LOWER_HALF(lh_info.fsaddr);
+  retval = NEXT_FUNC(Error_class)(errorcode, errorclass);
+  RETURN_TO_UPPER_HALF();
+  DMTCP_PLUGIN_ENABLE_CKPT();
+  return retval;
+}
+
+USER_DEFINED_WRAPPER(int, Error_string, (int) errorcode, (char *) string,
+                     (int *) resultlen)
+{
+  int retval;
+  DMTCP_PLUGIN_DISABLE_CKPT();
+  JUMP_TO_LOWER_HALF(lh_info.fsaddr);
+  retval = NEXT_FUNC(Error_string)(errorcode, string, resultlen);
+  RETURN_TO_UPPER_HALF();
+  DMTCP_PLUGIN_ENABLE_CKPT();
+  return retval;
+}
+
+PMPI_IMPL(int, MPI_Error_class, int errorcode, int *errorclass)
+PMPI_IMPL(int, MPI_Error_string, int errorcode, char *string,
+          int *resultlen)
+
