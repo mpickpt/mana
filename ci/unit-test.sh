96134219efe8466c1c46f82d5b87dd2261ca13b5
jackpao-memverge <54881195+jackpao-memverge@users.noreply.github.com>
2022-03-07 14:29:40 -0800

    Update unit-test.sh

--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -5,6 +5,12 @@ SCRIPT_DIR="$(dirname $(readlink -f "$0"))"
 
 cd $SCRIPT_DIR/..
 
+OS_VER=$(grep '^VERSION_ID' /etc/os-release | sed 's/"//g'| cut -d "=" -f2)
+#GCC 8 or higher
+if [[ $OS_VER == 7 ]]; then
+  source scl_source enable devtoolset-8
+fi
+
 ./configure
 make clean
 make -j 8 mana
