a2613a20d555bc7dd21e4273538ea65c8a8dbcb1
jackpao-memverge <54881195+jackpao-memverge@users.noreply.github.com>
2022-03-09 11:33:59 -0800

    Update unit-test.sh

--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -14,7 +14,7 @@ fi
 ./configure
 make clean
 make -j 8 mana
-if [ ! -f "bin/lh_proxy" -o \! -f "bin/mana_launch" ];then echo ERROR: make failed;exit 1;fi
+if [ \! -f "bin/lh_proxy" -o \! -f "bin/mana_launch" ];then echo ERROR: make failed;exit 1;fi
 
 cd $SCRIPT_DIR/../contrib/mpi-proxy-split/unit-test
 make || exit 1
