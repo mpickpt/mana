299cb5e6fa5dbafca5934b41e6af63dc385d8859
Twinkle Jain <jain.t@husky.neu.edu>
2022-06-15 03:43:26 -0400

    Updated Alloc_mem for regression test

--- mpi-proxy-split/test/Alloc_mem.c
+++ mpi-proxy-split/test/Alloc_mem.c
@@ -1,21 +1,31 @@
-#include "mpi.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
+#include <mpi.h>
+
+#define LOOP_COUNT 50
+#define CHAR_COUNT 100
 
 int main( int argc, char *argv[] )
 {
   char *mem;
-  int rank;
+  int rank, ret;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Alloc_mem(100 * sizeof(char), MPI_INFO_NULL, &mem);
-  printf("Rank %d allocated 100 chars at %p\n", rank, &mem);
-  fflush(stdout);
-  sleep(5);
-  MPI_Free_mem(mem);
-  printf("Rank %d freed %p\n", rank, &mem);
-  fflush(stdout);
+  for (int i = 0; i < LOOP_COUNT; i++) {
+    ret = MPI_Alloc_mem(CHAR_COUNT * sizeof(char), MPI_INFO_NULL, &mem);
+    assert(ret == MPI_SUCCESS);
+    printf("Rank %d allocated %d chars at %p\n", rank, CHAR_COUNT, &mem);
+    fflush(stdout);
+
+    sleep(2);
+
+    ret = MPI_Free_mem(mem);
+    assert(ret == MPI_SUCCESS);
+    printf("Rank %d freed %p\n", rank, &mem);
+    fflush(stdout);
+  }
   MPI_Finalize();
   return 0;
 }
