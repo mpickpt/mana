092f6f5edf05ff0b7c7da2a3c675b9a2ce19d9a8
Twinkle Jain <jain.t@husky.neu.edu>
2022-06-15 04:03:53 -0400

    Updated Allreduce test for regression

--- mpi-proxy-split/test/Allreduce_test.c
+++ mpi-proxy-split/test/Allreduce_test.c
@@ -6,9 +6,11 @@
 #include <assert.h>
 #include <stdlib.h>
 
+#define COUNT 1000
+
 int main(int argc, char *argv[])
 {
-  int count = 10;
+  int count = COUNT;
   int *in, *out, *sol;
   int i, fnderr=0;
   int rank, size;
@@ -16,36 +18,40 @@ int main(int argc, char *argv[])
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  in = (int *)malloc( count * sizeof(int) );
-  out = (int *)malloc( count * sizeof(int) );
-  sol = (int *)malloc( count * sizeof(int) );
-  for (i=0; i<count; i++)
-  {
-    *(in + i) = i;
-    *(sol + i) = i*size;
-    *(out + i) = 0;
-  }
-  MPI_Allreduce( in, out, count, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
-  for (i=0; i<count; i++)
-  {
-    printf("[Rank = %d] At index = %d: In = %d, Out = %d, Expected Out = %d \n",
-           rank, i, *(in + i), *(out + i), *(sol + i));
-    fflush(stdout);
-
-    assert((*(out + i) == *(sol + i)));
-    if (*(out + i) != *(sol + i))
+  in = (int *)malloc(count * sizeof(int));
+  out = (int *)malloc(count * sizeof(int));
+  sol = (int *)malloc(count * sizeof(int));
+  while (1) {
+    for (i=0; i<count; i++)
     {
-      fnderr++;
+      *(in + i) = i;
+      *(sol + i) = i*size;
+      *(out + i) = 0;
+    }
+    MPI_Allreduce(in, out, count, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    for (i=0; i<count; i++)
+    {
+      #ifdef DEBUG
+        printf("[Rank = %d] at index = %d: In = %d, Out = %d, Expected Out = %d\
+              \n", rank, i, *(in + i), *(out + i), *(sol + i));
+        fflush(stdout);
+      #endif
+
+      assert((*(out + i) == *(sol + i)));
+      if (*(out + i) != *(sol + i))
+      {
+        fnderr++;
+      }
+    }
+    if (fnderr)
+    {
+      fprintf(stderr, "(%d) Error for type MPI_INT and op MPI_SUM\n", rank);
+      fflush(stderr);
     }
   }
-  if (fnderr)
-  {
-    fprintf( stderr, "(%d) Error for type MPI_INT and op MPI_SUM\n", rank );
-    fflush(stderr);
-  }
-  free( in );
-  free( out );
-  free( sol );
+  free(in);
+  free(out);
+  free(sol);
   MPI_Finalize();
-  return fnderr;
+  return EXIT_SUCCESS;
 }
