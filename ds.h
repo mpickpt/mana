b5a789de67d5d2227066c2ab5ce622464273fbef
Tarun Malviya <malviya.t@northeastern.com>
2022-06-17 12:07:32 -0700

    CartesianInfo struct defined in ds.h.

--- ds.h
+++ ds.h
@@ -1,6 +1,11 @@
 #define MAX_PROCESSES 100
 #define MAX_CART_PROP_SIZE 10
 
+typedef struct CartesianInfo {
+  int comm_old_rank, comm_cart_rank;
+  int coordinates[MAX_CART_PROP_SIZE];
+} CartesianInfo;
+
 typedef struct CartesianProperties {
   int old_comm_size;
   int new_comm_size;
