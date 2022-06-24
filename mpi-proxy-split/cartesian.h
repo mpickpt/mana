0883e038510974b4512a3e4b976f278689d975b1
Tarun Malviya <tarunsmalviya@gmail.com>
2022-06-24 00:13:19 -0500

    Code updated as per the feedback shared.

--- mpi-proxy-split/cartesian.h
+++ mpi-proxy-split/cartesian.h
@@ -1,11 +1,21 @@
+#ifndef CARTESIAN_H
+#define CARTESIAN_H
+
 #define MAX_PROCESSES 100
 #define MAX_CART_PROP_SIZE 10
 
+// This struct is used to store mapping of <comm_old_rank>, <comm_cart_rank> and
+// <coordinates>.
 typedef struct CartesianInfo {
   int comm_old_rank, comm_cart_rank;
   int coordinates[MAX_CART_PROP_SIZE];
 } CartesianInfo;
 
+// This struct is used to store the cartesian communicator properties, i.e., the
+// arguments passed to the MPI_Cart_Create() API.
+// For reference,
+// int MPI_Cart_create(MPI_Comm comm_old, int ndims, const int dims[],
+//                    const int periods[], int reorder, MPI_Comm * comm_cart)
 typedef struct CartesianProperties {
   int comm_old_size;
   int comm_cart_size;
@@ -18,3 +28,4 @@ typedef struct CartesianProperties {
   int reorder;
 } CartesianProperties;
 
+#endif // ifndef CARTESIAN_H
