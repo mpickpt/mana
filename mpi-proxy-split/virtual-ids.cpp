c058d07051a6c4b132d1a02200082a3d91c2ed51
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-06-02 18:11:45 -0400

    more descriptor work

--- mpi-proxy-split/virtual-ids.cpp
+++ mpi-proxy-split/virtual-ids.cpp
@@ -35,12 +35,6 @@
 
 #define MAX_VIRTUAL_ID 999
 
-// perlmutter -- load more modules
-
-// load cray-pmi (process management i-)
-// unload cudatoolkit
-// unload gpu
-
 struct ggid_desc_t;
 union id_desc_t;
 
@@ -53,6 +47,8 @@ typedef typename std::map<int, ggid_desc_t*>::iterator ggid_desc_iterator;
 int base = 1;
 int nextvId = base;
 
+#define CONCAT(a,b) a ## b
+
 // Writing these macros as ternary expressions means there is no overhead associated with extra function arguments.
 #define DESCRIPTOR_TO_VIRTUAL(id, null) \ 
   (id == null) ? null : descriptorToVirtual(type)
@@ -61,7 +57,7 @@ int nextvId = base;
   (id == null) ? null : virtualToDescriptor(id)
 
 #define ADD_NEW(real_id, null, descriptor_type)						\
-  (real_id == null) ? null : onCreate(real_id, malloc(sizeof(descriptor_type)))
+  (real_id == null) ? null : assignVid(CONCAT(init_,descriptor_type)(real_id))
 
 #define REMOVE_OLD(virtual_id, null) \
   (virtual_id == null) ? null : onRemove(virtual_id)
@@ -86,7 +82,6 @@ int nextvId = base;
   VIRTUAL_TO_REAL(id, MPI_COMM_NULL)
 #define ADD_NEW_COMM(id) \
   (id == MPI_COMM_NULL) ? null : onCreateComm(id, malloc(sizeof(comm_desc_t))) // HACK. Communicators are special, we need to give a ggid.
-  // ADD_NEW(id, MPI_COMM_NULL, comm_desc_t)
 #define REMOVE_OLD_COMM(id) \
   REMOVE_OLD(id, MPI_COMM_NULL)
 #define UPDATE_COMM_MAP(v, r) \
@@ -186,90 +181,6 @@ struct comm_desc_t {
     // unsigned long target; // Target number for the CVC algorithm
 };
 
-struct group_desc_t {
-    MPI_Group real_id; // Real MPI group in the lower-half
-    int handle; // A copy of the int type handle generated from the address of this struct
-    int *ranks; // list of ranks of the group.
-    // unsigned int ggid; // Global Group ID
-};
-
-struct request_desc_t {
-    MPI_Request real_id; // Real MPI request in the lower-half
-    int handle; // A copy of the int type handle generated from the address of this struct
-    enum request_kind; // P2P request or collective request
-    MPI_Status status; // Real MPI status in the lower-half
-};
-
-struct op_desc_t {
-    MPI_Op real_id; // Real MPI operator in the lower-half
-    int handle; // A copy of the int type handle generated from the address of this struct
-    MPI_User_function *user_fn; // Function pointer to the user defined op function
-};
-
-struct datatype_desc_t {
-  // TODO add mpi type identifier field virtual class
-    MPI_Type real_id; // Real MPI type in the lower-half
-    int handle; // A copy of the int type handle generated from the address of this struct
-    // Components of user-defined datatype.
-    MPI_count num_integers;
-    int *integers;
-    MPI_count num_addresses;
-    int *addresses;
-    MPI_count num_large_counts;
-    int *large_counts;
-    MPI_count num_datatypes;
-    int *datatypes;
-    int *combiner;
-};
-
-union id_desc_t {
-    comm_desc_t comm;
-    group_desc_t group;
-    request_desc_t request;
-    op_desc_t op;
-    datatype_desc_t datatype;
-};
-
-// Given id_desc_t, return the contained virtualid.
-int descriptorToVirtual(id_desc_t* desc) {
-  return ((comm_desc_t*)desc)->handle;
-}
-
-// Given int virtualid, return the contained id_desc_t if it exists.
-// Otherwise return NULL
-id_desc_t* virtualToDescriptor(int virtId) {
-  id_desc_iterator it = idDescriptorTable.find(virtId);
-  if (it != idDescriptorTable.end()) {
-    return it->second;
-  }
-  return NULL;
-}
-
-// Given int virtualId and realId of MPI size, update the descriptor referenced by virtualid, if it exists.
-// TODO If the referenced virtualID does not exist, create a descriptor for it.
-// Returns a reference to the descriptor created.
-id_desc_t* updateMapping(int virtId, __typeof__(MPI_Comm) realId) { // HACK MPICH
-  id_desc_iterator it = idDescriptorTable.find(virtId);
-  if (it != idDescriptorTable.end()) {
-    id_desc_t* desc = idDescriptorTable[virtId]; // if doesn't exist?
-    ((comm_desc_t*)desc)->real_id = realId;
-    return desc;
-  } else {
-    // TODO
-    return NULL;
-  }
-}
-
-// Given realId of MPI size and id descriptor, fill out the id descriptor and save it in the table.
-// Returns the vId assigned.
-int onCreate(__typeof__(MPI_Comm) realId, id_desc_t* desc) { // HACK MPICH
-  int vId = nextvId++; // TODO
-  ((comm_desc_t*)desc)->real_id = realId;
-  ((comm_desc_t*)desc)->handle = vId;
-
-  idDescriptorTable[vId] = desc;
-  return vId;
-}
 
 // Hash function on integers. Consult https://stackoverflow.com/questions/664014/.
 // Returns a hash.
@@ -304,22 +215,158 @@ int getggid(MPI_Comm comm) {
   return ggid;
 }
 
-// Given a real MPI Communicator and id descriptor, fill out the descriptor with the ggid.
-// Then, proceed as with any other real id.
-// Returns the vId assigned.
-int onCreateComm(MPI_Comm realComm, comm_desc_t* desc) {
-  int vId = nextvId++;
-  int ggid = getggid(realId);
-  ggid_iterator it = ggidTable.find(ggid);
+comm_desc_t* init_comm_desc_t(MPI_Comm realComm) {
+  int ggid = getggid(realComm);
+  ggid_desc_iterator it = ggidDescriptorTable.find(ggid);
+  comm_desc_t* desc = ((comm_desc_t*)malloc(sizeof(comm_desc_t)));
 
-  if (it != ggidTable.end()) {
+  if (it != ggidDescriptorTable.end()) {
     desc->ggid_desc = it->second;
   } else {
     ggid_desc_t* gd = ((ggid_desc_t *) malloc(sizeof(ggid_desc_t)));
     gd->ggid = ggid;
-    ggidTable[ggid] = gd;
+    ggidDescriptorTable[ggid] = gd;
     desc->ggid_desc = gd;
   }
+  desc->real_id = realComm;
+  desc->size = 0;
+  desc->local_rank = 0;
+  desc->ranks = NULL;
+  return desc;
+}
+
+struct group_desc_t {
+    MPI_Group real_id; // Real MPI group in the lower-half
+    int handle; // A copy of the int type handle generated from the address of this struct
+    int *ranks; // list of ranks of the group.
+    // unsigned int ggid; // Global Group ID
+};
+
+group_desc_t* init_group_desc_t(MPI_Group realGroup) {
+  group_desc_t* desc = ((group_desc_t*)malloc(sizeof(group_desc_t)));
+  desc->real_id = realGroup;
+  desc->ranks = NULL;
+  return desc;
+}
+
+struct request_desc_t {
+    MPI_Request real_id; // Real MPI request in the lower-half
+    int handle; // A copy of the int type handle generated from the address of this struct
+    enum request_kind; // P2P request or collective request
+    MPI_Status status; // Real MPI status in the lower-half
+};
+
+request_desc_t* init_request_desc_t(MPI_Request realReq) {
+  request_desc_t* desc = ((request_desc_t*)malloc(sizeof(request_desc_t)));
+  desc->real_id = realReq;
+  desc->request_kind = NULL;
+  desc->status = 0;
+  return desc;
+}
+
+struct op_desc_t {
+    MPI_Op real_id; // Real MPI operator in the lower-half
+    int handle; // A copy of the int type handle generated from the address of this struct
+    MPI_User_function *user_fn; // Function pointer to the user defined op function
+};
+
+op_desc_t* init_op_desc_t(MPI_Op realOp) {
+  op_desc_t* desc = ((op_desc_t*)malloc(sizeof(op_desc_t)));
+  desc->real_id = realOp;
+  desc->user_fn = NULL;
+  return desc;
+}
+
+struct datatype_desc_t {
+  // TODO add mpi type identifier field virtual class
+    MPI_Type real_id; // Real MPI type in the lower-half
+    int handle; // A copy of the int type handle generated from the address of this struct
+    // Components of user-defined datatype.
+    MPI_count num_integers;
+    int *integers;
+    MPI_count num_addresses;
+    int *addresses;
+    MPI_count num_large_counts;
+    int *large_counts;
+    MPI_count num_datatypes;
+    int *datatypes;
+    int *combiner;
+};
+
+datatype_desc_t* init_datatype_desc_t(MPI_Type realType) {
+  datatype_desc_t* desc = ((datatype_desc_t*)malloc(sizeof(datatype_desc_t)));
+  desc->real_id = realType;
+
+  desc->num_integers = 0;
+  desc->integers = NULL;
+  desc->num_addresses = 0;
+  desc->addresses = NULL;
+  desc->num_large_counts = 0;
+  desc->large_counts = NULL;
+  desc->num_datatypes = 0;
+  desc->datatypes = NULL;
+  desc->combiner = NULL;
+  return desc;
+}
+
+union id_desc_t {
+    comm_desc_t comm;
+    group_desc_t group;
+    request_desc_t request;
+    op_desc_t op;
+    datatype_desc_t datatype;
+};
+
+id_desc_t* init_id_desc_t() {
+  int vId = nextvId++;
+  return NULL;
+}
+
+// Given id_desc_t, return the contained virtualid.
+int descriptorToVirtual(id_desc_t* desc) {
+  return ((comm_desc_t*)desc)->handle;
+}
+
+// Given int virtualid, return the contained id_desc_t if it exists.
+// Otherwise return NULL
+id_desc_t* virtualToDescriptor(int virtId) {
+  id_desc_iterator it = idDescriptorTable.find(virtId);
+  if (it != idDescriptorTable.end()) {
+    return it->second;
+  }
+  return NULL;
+}
+
+// Given int virtualId and realId of MPI size, update the descriptor referenced by virtualid, if it exists.
+// TODO If the referenced virtualID does not exist, create a descriptor for it.
+// Returns a reference to the descriptor created.
+id_desc_t* updateMapping(int virtId, __typeof__(MPI_Comm) realId) { // HACK MPICH
+  id_desc_iterator it = idDescriptorTable.find(virtId);
+  if (it != idDescriptorTable.end()) {
+    id_desc_t* desc = idDescriptorTable[virtId]; // if doesn't exist?
+    ((comm_desc_t*)desc)->real_id = realId;
+    return desc;
+  } else {
+    // TODO
+    return NULL;
+  }
+}
+
+// Given id desc, get vid, fill out the vid handle, and map the desc.
+// Returns the vId assigned.
+int assignVid(id_desc_t* desc) {
+  int vId = nextvId++; // TODO
+  ((comm_desc_t*)desc)->handle = vId;
+
+  idDescriptorTable[vId] = desc;
+  return vId;
+}
+
+// Given a real MPI Communicator and id descriptor, fill out the descriptor with the ggid.
+// Then, proceed as with any other real id.
+// Returns the vId assigned.
+int onCreateComm(MPI_Comm realComm, comm_desc_t* desc) {
+
   return onCreate(realComm, vType);
 }
 
