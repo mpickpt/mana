835e81a454d5bb9d035e64d8ac0786883915e730
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-06-09 22:09:05 -0400

    type stuff upd8

--- mpi-proxy-split/virtual-ids.h
+++ mpi-proxy-split/virtual-ids.h
@@ -29,10 +29,10 @@
 #define REMOVE_OLD(virtual_id, null) \
   (virtual_id == null) ? null : onRemove(virtual_id)
 
-#define UPDATE_MAP(virtual_id, to_update, null, descriptor_type)	\
+#define UPDATE_MAP(virtual_id, to_update, null, descriptor_type, to_update_type)	\
   ({ \
     id_desc_iterator _UM_it = idDescriptorTable.find(virtual_id); \
-    int toReturn; \
+    to_update_type toReturn; \
     if (_UM_it != idDescriptorTable.end()) { \
       descriptor_type* desc = ((descriptor_type*)idDescriptorTable[virtual_id]); \
       desc->real_id = to_update; \
@@ -55,7 +55,7 @@
 #define REMOVE_OLD_FILE(id) \
   REMOVE_OLD(id, MPI_FILE_NULL)
 #define UPDATE_FILE_MAP(v, r) \
-  UPDATE_MAP(v, r, MPI_FILE_NULL, file_desc_t)
+  UPDATE_MAP(v, r, MPI_FILE_NULL, file_desc_t, MPI_File)
 
 #define DESC_TO_VIRTUAL_COMM(id) \
   DESC_TO_VIRTUAL(id, MPI_COMM_NULL) 
@@ -68,7 +68,7 @@
 #define REMOVE_OLD_COMM(id) \
   REMOVE_OLD(id, MPI_COMM_NULL)
 #define UPDATE_COMM_MAP(v, r) \
-  UPDATE_MAP(v, r, MPI_COMM_NULL, comm_desc_t)
+  UPDATE_MAP(v, r, MPI_COMM_NULL, comm_desc_t, MPI_Comm)
 
 #define DESC_TO_VIRTUAL_GROUP(id) \
   DESC_TO_VIRTUAL(id, MPI_GROUP_NULL) 
@@ -81,7 +81,7 @@
 #define REMOVE_OLD_GROUP(id) \
   REMOVE_OLD(id, MPI_GROUP_NULL)
 #define UPDATE_GROUP_MAP(v, r) \
-  UPDATE_MAP(v, r, MPI_GROUP_NULL, group_desc_t)
+  UPDATE_MAP(v, r, MPI_GROUP_NULL, group_desc_t, MPI_Group)
 
 #define DESC_TO_VIRTUAL_TYPE(id) \
   DESC_TO_VIRTUAL(id, MPI_DATATYPE_NULL) 
@@ -94,7 +94,7 @@
 #define REMOVE_OLD_TYPE(id) \
   REMOVE_OLD(id, MPI_DATATYPE_NULL)
 #define UPDATE_TYPE_MAP(v, r) \
-  UPDATE_MAP(v, r, MPI_DATATYPE_NULL, datatype_desc_t)
+  UPDATE_MAP(v, r, MPI_DATATYPE_NULL, datatype_desc_t, MPI_Datatype)
 
 #define DESC_TO_VIRTUAL_OP(id) \
   DESC_TO_VIRTUAL(id, MPI_OP_NULL) 
@@ -107,7 +107,7 @@
 #define REMOVE_OLD_OP(id) \
   REMOVE_OLD(id, MPI_OP_NULL)
 #define UPDATE_OP_MAP(v, r) \
-  UPDATE_MAP(v, r, MPI_OP_NULL, op_desc_t)
+  UPDATE_MAP(v, r, MPI_OP_NULL, op_desc_t, MPI_Op)
 
 #define DESC_TO_VIRTUAL_COMM_KEYVAL(id) \
   DESC_TO_VIRTUAL(id, 0) 
@@ -120,7 +120,7 @@
 #define REMOVE_OLD_COMM_KEYVAL(id) \
   REMOVE_OLD(id, 0)
 #define UPDATE_COMM_KEYVAL_MAP(v, r) \
-  UPDATE_MAP(v, r, 0, comm_keyval_desc_t)
+  UPDATE_MAP(v, r, 0, comm_keyval_desc_t, int)
 
 #define DESC_TO_VIRTUAL_REQUEST(id) \
   DESC_TO_VIRTUAL(id, MPI_REQUEST_NULL) 
@@ -133,7 +133,7 @@
 #define REMOVE_OLD_REQUEST(id) \
   REMOVE_OLD(id, MPI_REQUEST_NULL)
 #define UPDATE_REQUEST_MAP(v, r) \
-  UPDATE_MAP(v, r, MPI_REQUEST_NULL, request_desc_t)
+  UPDATE_MAP(v, r, MPI_REQUEST_NULL, request_desc_t, MPI_Request)
 
 #ifndef NEXT_FUNC
 # define NEXT_FUNC(func)                                                       \
