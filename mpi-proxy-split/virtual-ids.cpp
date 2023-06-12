7d6822d137ab891470c02955592a1b164d41a553
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-06-12 15:43:49 -0400

    Begin filling out comm metadata

--- mpi-proxy-split/virtual-ids.cpp
+++ mpi-proxy-split/virtual-ids.cpp
@@ -57,15 +57,15 @@ int hash(int i) {
 // Compute the ggid [Global Group Identifier] of a real MPI communicator.
 // This consists of a hash of its integer ranks.
 // Returns ggid.
-int getggid(MPI_Comm comm) {
+int getggid(MPI_Comm comm, int worldRank, int commSize, int* rbuf) {
   if (comm == MPI_COMM_NULL) {
     return comm;
   }
   unsigned int ggid = 0;
-  int worldRank, commSize;
-  MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
-  MPI_Comm_size(comm, &commSize);
-  int rbuf[commSize];
+  // int worldRank, commSize;
+  // MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
+  // MPI_Comm_size(comm, &commSize);
+  // int rbuf[commSize];
 
   DMTCP_PLUGIN_DISABLE_CKPT();
   JUMP_TO_LOWER_HALF(lh_info.fsaddr);
@@ -82,6 +82,11 @@ int getggid(MPI_Comm comm) {
 }
 
 comm_desc_t* init_comm_desc_t(MPI_Comm realComm) {
+  int worldRank, commSize;
+  MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
+  MPI_Comm_size(realComm, &commSize);
+  int* ranks = ((int* )malloc(sizeof(int) * commSize));
+
   int ggid = getggid(realComm);
   ggid_desc_iterator it = ggidDescriptorTable.find(ggid);
   comm_desc_t* desc = ((comm_desc_t*)malloc(sizeof(comm_desc_t)));
@@ -95,9 +100,13 @@ comm_desc_t* init_comm_desc_t(MPI_Comm realComm) {
     desc->ggid_desc = gd;
   }
   desc->real_id = realComm;
-  desc->size = 0;
-  desc->local_rank = 0;
-  desc->ranks = NULL;
+  desc->size = commSize;
+
+  int localRank;
+  MPI_Comm_rank(realComm, &localRank);
+  desc->local_rank = localRank;
+
+  desc->ranks = ranks;
   return desc;
 }
 
