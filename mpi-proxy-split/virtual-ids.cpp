411dc73ef2343f578f0219a8001fe04c62b47445
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-06-16 12:56:52 -0400

    fflush

--- mpi-proxy-split/virtual-ids.cpp
+++ mpi-proxy-split/virtual-ids.cpp
@@ -234,6 +234,7 @@ void destroy_file_desc_t(file_desc_t* file) {
 
 void print_id_descriptors() {
   printf("Printing id_descriptors:\n");
+  fflush(stdout);
   for (id_desc_pair pair : idDescriptorTable) {
     printf("%i\n", pair.first);
   }
