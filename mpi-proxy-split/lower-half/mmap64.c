f49e8fd4fa129e4d9d174b72c0de537bbf87803a
Gene Cooperman <gene@ccs.neu.edu>
2023-06-21 02:59:02 -0400

    Make lower-half mmap/munmap more robust
     * libxpmem (and maybe other kernel drivers) can use ioctl to mmap
        additional memory segments, such as /dev/xpmem.  If they mmap
        many such segments or a few large ones, they can occupy a lot
        of the lower-half lh_memRange (aka lh_mem_range in split_process.cpp).
     * This can cause a problem either:  because the lh_memRange does not
        have enough empty space for the lower half to mmap; or more often
        when there are many upper-half libraries, whose mmap allocations
        grow downward and enter the lh_memRange.
     * The lower half adds MAP_FIXED to its mmap's, thinking that it controls
        all of lh_memRange, and trying to allocate its mmaps contiguously
        and growing upward in lh_memRange.  Its mmap can receive an ENOMEM
        if it tries to clobber /dev/xpmem.  It then abandons the mmap on
        the assumption that the caller to mmap (libmpi) would want to see
        the ENOMEM.  The libmpi caller can then test another region with
        mmap (maybe with mmap and a specified address or with mmap/MAP_FIXED
        and a specified address, followed by munmap).  The lower-half mmap
        adds the MAP_FIXED flag, as usual.  This can cause the lower-half
        mmap to clobber an upper-half mmap, if the upper-half mmap had
        been allocated in the lh_memRange.  (The upper-half mmap may enter
        the lh_memRange if the upper half has very many libraries, since
        libraries are populated using mmap without a suggested address,
        and those mmap's grow downward.)
     * The (partial) solution is for the lower-half mmap to use
        MAP_FIXED_NOREPLACE instead of MAP_FIXED, and test for EEXIST.
        Additionally, it may be necessary in a future commit to choose a
        different lh_memRange further away from the many upper-half libraries
        that are being mmap'ed.

--- mpi-proxy-split/lower-half/mmap64.c
+++ mpi-proxy-split/lower-half/mmap64.c
@@ -17,6 +17,7 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
+#include <linux/version.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
@@ -28,6 +29,8 @@
 #include "mmap_internal.h"
 #include "mpi_copybits.h"
 #include <sys/syscall.h>
+
+#define HAS_MAP_FIXED_NOREPLACE LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
 
 #ifndef __set_errno
 # define __set_errno(Val) errno = (Val)
@@ -59,13 +62,15 @@
 // Modified to keep track of regions mmaped by lower half
 
 // TODO:
-//  1. Make the size of list dynamic
+//  1. Add byte, 0b10101010, everywhere to lh_memRange, when first
+//     calling mmap within 'if (!initialized)' logic, below.
+//  2. Make the size of list dynamic
 
 // Number of valid objects in the 'mmaps' list below
 int numRegions = 0;
 
 // List of regions mmapped by the lower half
-MmapInfo_t mmaps[MAX_TRACK] = {0};
+MmapInfo_t mmaps[MAX_TRACK] = {{0}};
 
 // Pointer to the next free address used for allocation
 void *nextFreeAddr = NULL;
@@ -118,9 +123,7 @@ getNextAddr(size_t len)
   nextFreeAddr = (char*)curr + ROUND_UP(len) + PAGE_SIZE;
 
   // Assert if we have gone past the end of the lower half memory range
-  if (nextFreeAddr > lh_memRange.end) {
-    assert(0);
-  }
+  assert(nextFreeAddr <= lh_memRange.end);
   return curr;
 }
 
@@ -162,6 +165,38 @@ getNextHugeAddr(size_t len)
 void *
 __mmap64 (void *addr, size_t len, int prot, int flags, int fd, __off_t offset)
 {
+  static int initialized = 0;
+  if (!initialized) {
+    int length = lh_memRange.end - lh_memRange.start;
+# if 0
+    // FIXME: After we copy into upper half, call mmap as below.
+    //        But don't call this when lh_proxy first executes by itself.
+    //        So, we need to recognize if we are executing lh_proxy
+    //          or the target MPI application.
+# if HAS_MAP_FIXED_NOREPLACE
+    void *rc = mmap(lh_memRange.start, length,
+                    PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_FIXED_NOREPLACE,
+                    -1, 0);
+    if (rc == MAP_FAILED && errno == EEXIST) {
+      char msg[] = "*** Panic: MANA lower half: can't initialize lh_memRange\n";
+      write(2, msg, sizeof(msg)); assert(rc == 0);
+    }
+#else
+    void *rc = mmap(lh_memRange.start, length,
+                    PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    if (rc != lh_memRange.start) {
+      if (rc != MAP_FAILED) {
+        munmap(rc, length);
+      }
+      char msg[] = "*** Panic: MANA lower half: can't initialize lh_memRange\n";
+      write(2, msg, sizeof(msg)); assert(rc != lh_memRange.start);
+    }
+# endif
+# endif
+#endif
+    initialized = 1;
+  }
+
   void *ret = MAP_FAILED;
   MMAP_CHECK_PAGE_UNIT ();
 
@@ -192,11 +227,21 @@ __mmap64 (void *addr, size_t len, int prot, int flags, int fd, __off_t offset)
         // TODO: Check fd is pointing to hugetlbfs
         addr = getNextHugeAddr(len);
       } else {
+        // FIXME:  This appears to be for hugetlbfs with no backing store.
+        //         How is it used?
         addr = NULL;
       }
     }
+    // FIXME: Check if original caller had used MAP_FIXED
+    //          or MAP_FIXED_NOREPLACE; If so, we have a big problem here.
+    //          Abort and tell user for now??  Or just make the
+    //          call directly now, and hope for the best??
     if (addr) {
-      flags |= MAP_FIXED;
+#if HAS_MAP_FIXED_NOREPLACE
+      flags |= MAP_FIXED_NOREPLACE;
+#else
+      flags |= MAP_FIXED; // Dangerous!  We might clobber unknown mmap region.
+#endif
     }
   }
 
@@ -207,15 +252,44 @@ __mmap64 (void *addr, size_t len, int prot, int flags, int fd, __off_t offset)
     extraGuardPage = 1;
     totalLen += 2 * PAGE_SIZE;
   }
+  while (1) {
 #ifdef __NR_mmap2
-  ret = MMAP_CALL (mmap2, addr, totalLen, prot, flags, fd,
-            (off_t) (offset / MMAP2_PAGE_UNIT));
+    if (addr) {
+      int rc = munmap(addr, totalLen);
+      if (rc == -1) {
+        char msg[] = "*** Panic: MANA lower half: can't munmap a region.\n";
+        write(2, msg, sizeof(msg)); assert(rc == 0);
+      }
+    }
+# define MMAP mmap2
+    ret = (void *) MMAP_CALL (MMAP, addr, totalLen, prot, flags, fd,
+                                    (off_t) (offset / MMAP2_PAGE_UNIT));
 #else
-  ret = (void *) MMAP_CALL (mmap, addr, totalLen, prot, flags, fd, offset);
+# define MMAP mmap
+    ret = (void *) MMAP_CALL (MMAP, addr, totalLen, prot, flags, fd, offset);
 #endif
 
+    if (ret == MAP_FAILED && errno == EEXIST) {
+      // Someone stole our mmap slot (via ioctl?).  This can happen if
+      //   a network or shared-memory library (e.g. xpmem) uses ioctl
+      //   to allocate memory.  Look for the next available address.
+      do { addr += PAGE_SIZE;
+         }
+      while ((void *)MMAP_CALL(MMAP, addr, PAGE_SIZE, PROT_NONE, MAP_PRIVATE,
+                               0, -1)
+             == MAP_FAILED);
+      nextFreeAddr = addr;
+      // FIXME:  We need to recognize dontuse everywhere.
+      mmaps[getMmapIdx(ret)].dontuse = 1; // Skip past the stolen region.
+      mmaps[getMmapIdx(ret)].len = addr-ret; // Set length of stolen region.
+      addr = getNextAddr(len); // FIXME: Maybe we want getNextHugeAddr()
+    } else {
+      break;
+    }
+  }  // end of while loop using MMAP_CALL
+
   // Accounting of the lower-half mmap regions
-  // XXX: Why are we doing this? Given that all the lower half memory
+  // XXX (Rohan): Why are we doing this? Given that all the lower half memory
   //      allocations are restricted to a specified range, do we need to do
   //      this?
   if (ret != MAP_FAILED) {
@@ -223,6 +297,7 @@ __mmap64 (void *addr, size_t len, int prot, int flags, int fd, __off_t offset)
     if (idx != -1) {
       mmaps[idx].len = ROUND_UP(len);
       mmaps[idx].unmapped = 0;
+      mmaps[idx].dontuse = 0;
     } else {
       int idx2 = extendExistingMmap(ret);
       if (idx2 != -1) {
@@ -236,6 +311,14 @@ __mmap64 (void *addr, size_t len, int prot, int flags, int fd, __off_t offset)
         mmaps[numRegions].unmapped = 0;
         idx = numRegions;
         numRegions = (numRegions + 1) % MAX_TRACK;
+        // FIXME: A better fix, below, is to set nextFreeAddr back to the start,
+        //   and then to test the size of any gap.  That can be done
+        //   by checking each known region.  Hopefully, there are not many.
+        //   Or at least, we can re-use any unmapped regions.
+        if (numRegions == 0) {
+          char msg[] = "*** Panic: MANA lower half: no more space for mmap.\n";
+          write(2, msg, sizeof(msg)); assert(numRegions > 0);
+        }
       }
     }
     if (extraGuardPage) {
