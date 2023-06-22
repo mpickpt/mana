e10fca33035b3463100aeb25f652d5c6eb80babb
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-06-22 18:44:28 -0400

    debug

--- mpi-proxy-split/seq_num.cpp
+++ mpi-proxy-split/seq_num.cpp
@@ -199,7 +199,7 @@ void commit_begin(MPI_Comm comm, bool passthrough) {
   }
   pthread_mutex_lock(&seq_num_lock);
   current_phase = IN_CS;
-  printf("Enter desc-check\n");
+  printf("Enter desc-check, comm is %x\n", comm);
   fflush(stdout);
   ggid_desc_t* comm_ggid_desc = VIRTUAL_TO_DESC_COMM(comm)->ggid_desc;
     comm_ggid_desc->seq_num++;
