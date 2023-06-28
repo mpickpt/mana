528bcce6cbf3d312f5fe59ac2fddcc929edd33e8
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-06-28 16:52:04 -0400

    ifdef

--- mpi-proxy-split/virtual-ids.cpp
+++ mpi-proxy-split/virtual-ids.cpp
@@ -118,7 +118,7 @@ comm_desc_t* init_comm_desc_t(MPI_Comm realComm) {
 void update_comm_desc_t(comm_desc_t* desc) {
   MPI_Group group;
   MPI_Comm_group(desc->real_id, &group);
-#ifdef
+#ifdef DEBUG_VIDS
   printf("update_comm_desc group: %x", group);
   fflush(stdout);
 #endif
