58c2e6628a32f24fc8fa42f1a2da6265fd1d6cd7
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-08-14 18:35:43 -0400

    Working on PRESUSPEND bug

--- mpi-proxy-split/virtual-ids.h
+++ mpi-proxy-split/virtual-ids.h
@@ -338,4 +338,6 @@ int hash(int i);
 
 void init_comm_world(); // This is required for the CVC algorithm.
 
+MPI_Comm get_vcomm_internal(MPI_comm realComm); // As is this.
+
 #endif // ifndef VIRTUAL_ID_H
