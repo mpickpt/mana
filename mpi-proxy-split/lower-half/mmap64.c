c9b5919bee6308e63eca8dd5cfd56b06aa6174a5
Gene Cooperman <gene@ccs.neu.edu>
2023-08-17 13:18:04 -0400

    Improve documentation of reserving lh_memRange

--- mpi-proxy-split/lower-half/mmap64.c
+++ mpi-proxy-split/lower-half/mmap64.c
@@ -42,12 +42,6 @@
 //           memory regions using mmaps[] _and_ shms[].
 //   FIXME:  We do not catch any changes in /proc/PID/maps due to ioctl.
 
-// FIXME: When we know this works, remove this preprocessor variable and make
-// this permanent.  Right now, this causes us to reserve the lh_mem_range
-// in two places (during launch and during restart).  We should refactor
-// to do this in only one place.
-#define RESERVE_LH_MEM_RANGE
-
 #define HAS_MAP_FIXED_NOREPLACE LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
 
 #ifdef __NR_mmap2
@@ -141,22 +135,19 @@ resetMmappedList()
   }
   numRegions = 0;
 
-#ifdef RESERVE_LH_MEM_RANGE
-  // We will reserve the lh_memRange memory, so no one can steal it from us.
+  /**************************************************************************
+   * Next, We reserve the lh_memRange memory, so no one can steal it from us.
+   * The upper half (../mtcp_split_process.c) sets lh_memRange.start to
+   *   0x2aab00000000, and we assume that no one else uses that address range.
+   * NPTE: We reserve the lh_memRange memory in two places: launch and restart.
+   **************************************************************************/
   size_t length = lh_memRange.end - lh_memRange.start;
 #if HAS_MAP_FIXED_NOREPLACE
   // Use inline syscall here, to avoid our __mmap64 wrapper.
-  // FIXME:  Keep only the "dev/zero" '#else' when it's been tested.
-# if 0
-  void *rc = LH_MMAP_CALL(lh_memRange.start, length,
-                          PROT_WRITE,
-                          MAP_PRIVATE|MAP_ANONYMOUS|MAP_FIXED_NOREPLACE, -1, 0);
-# else
   int fd = open("/dev/zero", O_RDONLY);
   void *rc = LH_MMAP_CALL(lh_memRange.start, length,
                           PROT_NONE,
                           MAP_SHARED|MAP_ANONYMOUS|MAP_FIXED_NOREPLACE, fd, 0);
-# endif
   if (rc == MAP_FAILED) {
     char msg[] = "*** Panic: MANA lower half: can't reserve lh_memRange"
                  " using MAP_FIXED_NOREPLACE\n";
@@ -181,7 +172,6 @@ resetMmappedList()
   //         only later, when mmap64 takes a piece.of it.
   // memset(lh_memRange.start, CANARY_BYTE, length);
   mprotect(lh_memRange.start, length, PROT_NONE);
-#endif
 }
 
 int
