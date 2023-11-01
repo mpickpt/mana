c55b799c8f140c1e79c71f620afb65c48f90ddfa
Yao Xu <syxuyao@outlook.com>
2023-11-01 08:33:24 -0700

    Add incremented lh_proxy heap to lh_regions_list

--- mpi-proxy-split/lower-half/sbrk.c
+++ mpi-proxy-split/lower-half/sbrk.c
@@ -19,6 +19,10 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#include "mmap_internal.h"
 
 /* Defined in brk.c.  */
 extern void *__curbrk;
@@ -33,6 +37,19 @@ void *__endOfHeap = 0;
 #define PAGE_SIZE 4096
 
 #define ROUND_UP(addr) ((unsigned long)(addr + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
+
+#ifndef __set_errno
+# define __set_errno(Val) errno = (Val)
+#endif
+
+#ifdef __NR_mmap2
+# define LH_MMAP_CALL(addr, len, prot, flags, fd, offset) \
+           (void *)MMAP_CALL(mmap2, addr, len, prot, flags, fd, \
+                                    (off_t) (offset / MMAP2_PAGE_UNIT))
+#else
+# define LH_MMAP_CALL(addr, len, prot, flags, fd, offset) \
+           (void *)MMAP_CALL(mmap, addr, len, prot, flags, fd, offset)
+#endif
 
 /* Extend the process's data space by INCREMENT.
    If INCREMENT is negative, shrink data space by - INCREMENT.
@@ -69,10 +86,16 @@ __sbrk (intptr_t increment)
   //   return (void *) -1;
 
   if (oldbrk + increment > __endOfHeap) {
-    if (mmap(__endOfHeap, ROUND_UP(oldbrk + increment - __endOfHeap),
+    if (LH_MMAP_CALL(__endOfHeap, ROUND_UP(oldbrk + increment - __endOfHeap),
              PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS,
              -1, 0) < 0) {
        return (void *) -1;
+    }
+    if (lh_info.lh_regions_list != NULL) {
+      lh_info.lh_regions_list[lh_info.numCoreRegions].start_addr = __endOfHeap;
+      lh_info.lh_regions_list[lh_info.numCoreRegions].end_addr = ROUND_UP(oldbrk + increment);
+      lh_info.lh_regions_list[lh_info.numCoreRegions].prot = PROT_READ | PROT_WRITE;
+      lh_info.numCoreRegions++;
     }
   }
 
