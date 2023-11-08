29eb1c0e6db358e753362136a5a266e7d6a32d64
Gene Cooperman <gene@ccs.neu.edu>
2023-11-08 07:30:57 -0800

    Update sbrk, after libc_start_main, freeze endHeap

--- mpi-proxy-split/lower-half/sbrk.c
+++ mpi-proxy-split/lower-half/sbrk.c
@@ -15,6 +15,29 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
+/****************************************************************************
+ *   Copyright (C) 2019-2023 by Kapil AryaGene Cooperman, Rohan Garg, Yao Xu*
+ *   kapil.arya.17@gmail.com, gene@ccs.neu.edu, rohgarg@ccs.neu.edu,        *
+ *     xu.yao1@northeastern.edu                                             *
+ *                                                                          *
+ *  This file is part of DMTCP.                                             *
+ *                                                                          *
+ *  DMTCP is free software: you can redistribute it and/or                  *
+ *  modify it under the terms of the GNU Lesser General Public License as   *
+ *  published by the Free Software Foundation, either version 3 of the      *
+ *  License, or (at your option) any later version.                         *
+ *                                                                          *
+ *  DMTCP is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU Lesser General Public License for more details.                     *
+ *                                                                          *
+ *  You should have received a copy of the GNU Lesser General Public        *
+ *  License in the files COPYING and COPYING.LESSER.  If not, see           *
+ *  <http://www.gnu.org/licenses/>.                                         *
+ ****************************************************************************/
+
+#include <assert.h>
 #include <errno.h>
 #include <stdint.h>
 #include <unistd.h>
@@ -33,31 +56,44 @@ extern int __brk (void *addr);
 static int __libc_multiple_libcs = 0;
 
 void *__endOfHeap = 0;
-int endOfHeapFrozen = 0; // When we set this, sbrk will always return -1.
+int endOfHeapFrozen = 0; // When we set this, sbrk will always return -1
 
 #define PAGE_SIZE 4096
 
 #define ROUND_UP(addr) ((unsigned long)(addr + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
 
-#ifndef __set_errno
-# define __set_errno(Val) errno = (Val)
-#endif
-
-#ifdef __NR_mmap2
-# define LH_MMAP_CALL(addr, len, prot, flags, fd, offset) \
-           (void *)MMAP_CALL(mmap2, addr, len, prot, flags, fd, \
-                                    (off_t) (offset / MMAP2_PAGE_UNIT))
-#else
-# define LH_MMAP_CALL(addr, len, prot, flags, fd, offset) \
-           (void *)MMAP_CALL(mmap, addr, len, prot, flags, fd, offset)
-#endif
-
 /* Extend the process's data space by INCREMENT.
    If INCREMENT is negative, shrink data space by - INCREMENT.
    Return start of new space allocated, or -1 for errors.  */
 void *
-__sbrk (intptr_t increment)
+__sbrk(intptr_t increment)
 {
+  // uh sets lh_info.endOfHeapFrozenAddr after libc_start_main finixhes.
+  if (endOfHeapFrozen) {
+    /* We always return a failure unless the increment is 0 (to ask to current
+     * end-of-break). Malloc libraries handle -1 by initializing new arenas.
+     */
+    static int firstTime = 1;
+    if (firstTime) {
+      firstTime = 0;
+      // Mmap a page at the end of the break in case glibc tries to extend heap
+      // NOTE:  This is needed for mtcp_restart, lh_proxy, and the upper half.
+      // Rationale:  On restart, the end-of-heap is not here, but at
+      //             the end-of-heap for mtcp_restart.  Luckily, glibc
+      //             caches 'sbrk(0)', which is here.  So, glibc should
+      //             detect that there is an mmap'ed region just beyond it,
+      //             thus causing glibc to allocate a second arena elsewhere.
+      assert(MAP_FAILED != LH_MMAP_CALL(__curbrk, 4096, PROT_NONE,
+                                        MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED,
+                                        -1, 0));
+    }
+    if (increment == 0) {
+      return __curbrk;
+    } else {
+      return -1;
+    }
+  }
+
   void *oldbrk;
 
   /* If this is not part of the dynamic library or the library is used
@@ -66,7 +102,7 @@ __sbrk (intptr_t increment)
      instances of __brk and __sbrk can share the heap, returning
      interleaved pieces of it.  */
   if (__curbrk == NULL || __libc_multiple_libcs)
-    if (__brk (0) < 0)		/* Initialize the break.  */
+    if (__brk (0) < 0)		/* Initialize the break, __curbrk.  */
       return (void *) -1;
     else
       __endOfHeap = __curbrk;
