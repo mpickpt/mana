059f1d1f1b4f080b5615d212d103dcc12f54eece
Yao Xu <syxuyao@outlook.com>
2024-04-23 14:12:39 -0700

    Remove print in lower half dlsym

--- mpi-proxy-split/lower-half/lh_func_ptr.c
+++ mpi-proxy-split/lower-half/lh_func_ptr.c
@@ -12,6 +12,5 @@ void* lh_dlsym(enum MPI_Fncs fnc) {
   if (fnc < MPI_Fnc_NULL || fnc > MPI_Fnc_Invalid) {
     return NULL;
   }
-  printf("MPI function: %s, table addr: %p, function adr: %p\n", MPI_Fnc_strings[fnc], &MPI_Fnc_Ptrs, MPI_Fnc_Ptrs[fnc]);
   return MPI_Fnc_Ptrs[fnc];
 }
