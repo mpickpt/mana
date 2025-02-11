1e71290b6a239bfb074a8c1f253c586044a6cfab
Rajat Bisht <rajatpratapbisht@gmail.com>
2025-02-11 15:18:05 -0500

    Added header comment to MPI_Double_Int_test

--- mpi-proxy-split/test/MPI_Double_Int_test.c
+++ mpi-proxy-split/test/MPI_Double_Int_test.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <mpi.h>
 
+/*
+ * MPICH treats MPI_DOUBLE_INT  as a negative 32-bit integer, 
+ * signifying its 'struct' nature.
+ * MANA has defined look-up table for pre-defined MPI_Datatypes as 
+ * map<int64_t, int64_t> to accomodate for OpenMPI library.
+ *
+ * But, When handeling negative 64-bit value in MPICh with int32_t MPI_Datatypes, 
+ * conversion back to 64-bit results in loss of upper-bits, violating integrity of argument passed.
+ *
+ * This tests for a possible regression.
+ */
+
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
 
